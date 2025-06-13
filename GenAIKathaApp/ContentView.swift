c7cb99cf681c8171c5e3a6ff23a6ef58161ba678
ML <ragul.lakshmanan@gmail.com>
2025-06-13 16:20:47 +0530

    Removing commented code for github actions

--- GenAIKathaApp/ContentView.swift
+++ GenAIKathaApp/ContentView.swift
@@ -62,9 +62,6 @@ struct ContentView: View {
                     prefetchRoutesIfNeeded(for: user)
                 }
             }
-//    }
-//    .modelContainer(sharedModelContainer)
-//        }
         .modelContainer(sharedModelContainer)
     }
 
